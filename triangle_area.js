d08c2b3f4afd6295ef568514508894a4f5322c1d
PENTAKOTA V S SRAVAN KUMAR <89958709+skmprb@users.noreply.github.com>
2022-01-28 13:15:56 +0530

    Add files via upload

---
+++ triangle_area.js
@@ -0,0 +1,6 @@
+var h = parseInt(prompt("enter the height of the triangle : "));
+var b = parseInt(prompt("enter the base of the triangle : "));
+
+var area = (h * b)/2;
+
+console.log(" the area of the triangle is : "+area);
