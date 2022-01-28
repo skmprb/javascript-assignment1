930ef01d85653261c0e90887e7335b56be0e0d04
PENTAKOTA V S SRAVAN KUMAR <89958709+skmprb@users.noreply.github.com>
2022-01-28 13:21:29 +0530

    Add files via upload

---
+++ numcheck.js
@@ -0,0 +1,9 @@
+var num = parseInt(prompt("enter the number"));
+
+if(num > 0){
+    console.log("the number is positive");
+}else if(num == 0 ){
+    console.log("the number is zero");
+}else {
+    console.log("the number is negative");
+}
